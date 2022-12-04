c55b87beef300ee872914bc84313f2db0f0e96fe
ajakubauskas <aivaras@traveltime>
2022-12-04 19:18:19 +0200

    some implementation of hands: rock paper scissors

--- task02/src/main/scala/Main.scala
+++ task02/src/main/scala/Main.scala
@@ -25,3 +25,29 @@ object Main {
   }
 
 }
+
+
+sealed trait Hand {
+  def isWin(other: Hand): Boolean = (this, other) match {
+    case (Rock, Scissors) => true
+    case (Paper, Rock) => true
+    case (Scissors, Paper) => true
+    case _ => false
+  }
+  def isDraw(other: Hand): Boolean = this == other
+}
+
+object Rock extends Hand
+object Paper extends Hand
+object Scissors extends Hand
+
+object Hand {
+  val handMap = Map(
+    "A" -> Rock,
+    "B" -> Paper,
+    "C" -> Scissors,
+    "X" -> Rock,
+    "Y" -> Paper,
+    "Z" -> Scissors
+  )
+}
